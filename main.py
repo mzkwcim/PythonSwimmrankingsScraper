fba50c9e557d2cc6e5315486266df961288de466
M5kwc1m <144521687+mzkwcim@users.noreply.github.com>
2024-01-26 09:31:30 +0100

    Update main.py
    i've removed lap times from this basic scraper

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def contains(whole_text, text):
+    if text in whole_text:
+        return True
+    else:
+        return False
+
+
 url = ("https://www.swimrankings.net/index.php?page=rankingDetail&clubId=65773&gender=1"
        "&course=SCM&agegroup=0&stroke=0&season=-1")
 
@@ -10,12 +18,15 @@ if response.status_code == 200:
     html_content = response.text
     soup = BeautifulSoup(html_content, 'html.parser')
 
+    swimstyle_text = soup.select('td.swimstyle')
     time_text = soup.select('td.time')
     name_text = soup.select('td.fullname')
 
     for i in range(len(name_text)):
-        if name_text[i].getText(strip=True) is not None:
-            print(f"{str(name_text[i].getText(strip=True)).title().replace(',','')} {time_text[i].getText(strip=True)}")
+        if contains(swimstyle_text[i].getText(strip=True), "Lap") is False:
+            print(f"{str(swimstyle_text[i].getText(strip=True)).title()} "
+                  f"{str(name_text[i].getText(strip=True)).title().replace(',', '')} "
+                  f"{time_text[i].getText(strip=True)}")
 
 else:
-    print("Błąd przy pobieraniu strony. Status code:", response.status_code)
+    print("Error. Status code:", response.status_code)
